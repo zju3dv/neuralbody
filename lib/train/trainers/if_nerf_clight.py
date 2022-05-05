7e2419b8b64a4bc7af5999958a69939784699f51
Tancrede Guillou <tancredeguillou@gmail.com>
2022-05-05 19:03:31 +0200

    using lpips on whole images

--- lib/train/trainers/if_nerf_clight.py
+++ lib/train/trainers/if_nerf_clight.py
@@ -4,6 +4,8 @@ import torch
 from lib.networks.renderer import if_clight_renderer
 from lib.train import make_optimizer
 import lpips
+import cv2
+import numpy as np
 
 
 class NetworkWrapper(nn.Module):
@@ -27,7 +29,45 @@ class NetworkWrapper(nn.Module):
 
         mask = batch['mask_at_box']
         img_loss = self.img2mse(ret['rgb_map'][mask], batch['rgb'][mask])
-        img_lpips = self.lpips.forward(ret['rgb_map'][mask], batch['rgb'][mask])
+
+        ########################################## LPIPS PREP ##########################################
+
+        rgb_pred = ret['rgb_map'][0]
+        rgb_gt = batch['rgb'][0]
+
+        mask_at_box = batch['mask_at_box'][0]
+        H, W = int(cfg.H * cfg.ratio), int(cfg.W * cfg.ratio)
+        mask_at_box = mask_at_box.reshape(H, W)
+        # convert the pixels into an image
+        white_bkgd = int(cfg.white_bkgd)
+        img_pred = torch.zeros((H, W, 3)) + white_bkgd
+        img_pred[mask_at_box] = rgb_pred
+        img_gt = torch.zeros((H, W, 3)) + white_bkgd
+        img_gt[mask_at_box] = rgb_gt
+
+        #img_loss = np.mean((rgb_pred - rgb_gt)**2)
+
+        rgb_pred = img_pred
+        rgb_gt = img_gt
+        
+        # crop the object region
+        mask_at_box_np = batch['mask_at_box'][0].detach().cpu().numpy()
+        x, y, w, h = cv2.boundingRect(mask_at_box_np.astype(torch.uint8))
+        img_pred = img_pred[y:y + h, x:x + w]
+        img_gt = img_gt[y:y + h, x:x + w]
+        
+        img_pred = (img_pred[..., [2, 1, 0]] * 2) - 1
+        img_gt = (img_gt[..., [2, 1, 0]] * 2) - 1
+
+        # Transform (H, W, 3) in (1, 3, H, w)
+        img_pred = img_pred.permute(2, 0, 1)[None, :, :, :]
+        img_gt = img_gt.permute(2, 0, 1)[None, :, :, :]
+
+        # compute lpips
+        img_lpips = self.lpips.forward(img_pred, img_gt)
+
+        ########################################## LPIPS PREP ##########################################
+
         scalar_stats.update({'img_loss': img_loss})
         loss += (0.2 * img_loss + img_lpips)
 
