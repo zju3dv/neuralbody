333026fc12f33d5e732008e7094b442a0095a1e2
pengsida <pengsida@zju.edu.cn>
2022-06-30 17:04:29 +0800

    Update spconv.sh

--- docker/spconv.sh
+++ docker/spconv.sh
@@ -1,4 +1,5 @@
 cd spconv
 git checkout abf0acf30f5526ea93e687e3f424f62d9cd8313a
+git submodule update --init --recursive
 python setup.py bdist_wheel
 pip install dist/spconv-1.2.1-cp38-cp38-linux_x86_64.whl
